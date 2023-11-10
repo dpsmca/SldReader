0ec6cdcd82ea4de8b58a5087bc298de9104bc85d
David Sargeant <Sargeant.David@mayo.edu>
2023-11-10 14:42:23 -0600

    Updates from Keesh to fix access

--- SldReader.cs
+++ SldReader.cs
@@ -2,9 +2,13 @@
 // Console.WriteLine("Hello, World!");
 
 using System;
+using System.Collections.Generic;
 using ThermoFisher.CommonCore.Data.Business;
 using ThermoFisher.CommonCore.Data.Interfaces;
 using ThermoFisher.CommonCore.RawFileReader;
+
+using StringOrderedDictionary = System.Collections.Specialized.StringDictionary;
+
 
 namespace SldReader
 {
@@ -21,6 +25,37 @@ namespace SldReader
             string sldFilePath = args[0];
             ReadAndPrintSamples(sldFilePath);
         }
+
+        // returns an empty dictionary on errors
+        private static StringOrderedDictionary SLDReadSamples(string sldFilePath)
+        {
+            StringOrderedDictionary rawFilesAcquired = new StringOrderedDictionary();
+            // Initialize the SLD file reader
+            var sldFile = SequenceFileReaderFactory.ReadFile(sldFilePath);
+            if (sldFile.IsError)
+            {
+                Console.WriteLine($"Error opening the SLD file: {sldFilePath}, {sldFile.FileError.ErrorMessage}");
+                return rawFilesAcquired;
+            }
+            if (!(sldFile is ISequenceFileAccess))
+            {
+                Console.WriteLine($"This file {sldFilePath} does not support sequence file access.");
+                return rawFilesAcquired;
+            }
+            foreach (var sample in sldFile.Samples)
+            {
+                // I saw some .raw files in a FreeStyle SLD file and will skip these.
+                if (string.IsNullOrEmpty(sample.RawFileName))
+                {
+                    continue;
+                }
+                // Sometimes a path sep and sometimes not
+                string rawFileName = sample.Path.TrimEnd('\\') + Path.DirectorySeparatorChar + sample.RawFileName + ".raw";
+                rawFilesAcquired[rawFileName] = "No";
+            }
+            // According to docs, the file is not kept open.
+            return rawFilesAcquired;
+        }  // SLDReadSamples()
 
         private static void ReadAndPrintSamples(string sldFilePath)
         {
